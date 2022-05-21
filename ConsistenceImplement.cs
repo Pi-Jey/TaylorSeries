aaf06fea0701cba14319e892346eda5a3bade07f
pmixe <pmixe@DESKTOP-NMRV6RU>
2022-05-21 13:25:15 +0300

    Add project files.

---
+++ ConsistenceImplement.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaylorSeries
+{
+    internal class ConsistenceImplement
+    {
+        public ConsistenceImplement()
+        {
+
+        }
+        public double Factorial(uint num)
+        {
+            if (num <= 1)
+            {
+                return 1d;
+            }
+
+            return num * Factorial(num - 1);
+        }
+
+        public double Power(double num, int pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+
+            return num * Power(num, pow - 1);
+        }
+
+        public double Exp(double x, int n = 0, double precision = 1e-10)
+        {
+            var current = Power(x, n) / Factorial((uint)n);
+            if (current < precision)
+            {
+                return current;
+            }
+
+            return current + Exp(x, n + 1, precision);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static double Factorial(uint num)
+    {
+        if (num <= 1)
+        {
+            return 1d;
+        }
+
+        return num * Factorial(num - 1);
+    }
+
+    static double Power(double num, int pow)
+    {
+        if (pow == 0)
+        {
+            return 1;
+        }
+
+        return num * Power(num, pow - 1);
+    }
+
+    static double Exp(double x, int n = 0, double precision = 1e-10)
+    {
+        var current = Power(x, n) / Factorial((uint)n);
+        if (current < precision)
+        {
+            return current;
+        }
+
+        return current + Exp(x, n + 1, precision);
+    }
+
+    public static void Main()
+    {
+        Console.Write("x = ");
+        var x = double.Parse(Console.ReadLine());
+        var result = Exp(x);
+        Console.WriteLine("Exp(x)      = {0}", result);
+        Console.WriteLine("Math.Exp(x) = {0}", Math.Exp(x));
+        Console.ReadKey(true);
+    }
+}
